a542dfec3cdad153cdc6b177c15ada9fb0f5d35d
SK <sugiarto.kosasi@gmail.com>
2019-05-18 23:51:31 +0700

    Initial Commit

---
+++ NC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1
+//
+//  Created by Sugiarto Kosasi on 18/05/19.
+//  Copyright © 2019 SK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
