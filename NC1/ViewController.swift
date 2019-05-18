c4ac93803ca7f6e001810fdce77677ff2d967447
SK <sugiarto.kosasi@gmail.com>
2019-05-19 00:56:37 +0700

    create shape, movement

--- NC1/ViewController.swift
+++ NC1/ViewController.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var Button: UIButton!
     
 
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+        print(view.frame.maxX)
+        print(view.frame.maxY)
+        print(view.frame.minX)
+        print(view.frame.minY)
+        
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Button.layer.cornerRadius = 50
+        Button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
     }
-
-
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        var duration : Double = 2
+    
+        UIView.animate(withDuration: TimeInterval(duration)) {
+            self.Button.frame = CGRect(x: CGFloat(Float.random(in: 1..<200)), y: CGFloat(Float.random(in: 1..<800)), width: 100, height: 100)
+           
+            duration -= 0.5
+            
+            if duration == 0 {
+                self.Button.layer.cornerRadius = 0
+                self.Button.frame.size = CGSize(width: 300, height: 300)
+                self.Button.setTitle("WIN", for: .normal)
+            }
+        }
+    }
 }
 
